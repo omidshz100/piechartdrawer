a29d64f2e55f38a30877f977492b931f04af9e3e
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-06-13 10:04:39 +0200

    Done ✅☕️

--- ChartPie/ContentView.swift
+++ ChartPie/ContentView.swift
@@ -42,37 +42,6 @@ struct ContentView: View {
         }
     }
 }
-
-struct PieChartView: View {
-    @Binding var slices: [(Double, Color)]
-    @Binding var total:Double
-    @Binding var gap:Double
-    var body: some View {
-         Canvas { context, size in
-             // for size of each pie declair ....
-            let total = Double(total)//slices.reduce(0) { $0 + $1.0 }
-            context.translateBy(x: size.width * 0.5, y: size.height * 0.5)
-            var pieContext = context
-            pieContext.rotate(by: .degrees(-90))
-            let radius = min(size.width, size.height) * 0.48
-            let gapSize = Angle(degrees: gap) // size of the gap between slices in degrees
-            
-            var startAngle = Angle.zero
-            for (value, color) in slices {
-                let angle = Angle(degrees: 360 * (value / total))
-                let endAngle = startAngle + angle - gapSize
-                let path = Path { p in
-                    p.move(to: .zero)
-                    p.addArc(center: .zero, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
-                    p.closeSubpath()
-                }
-                pieContext.fill(path, with: .color(color))
-                startAngle = endAngle + gapSize
-            }
-        }
-        .aspectRatio(1, contentMode: .fit)
-    }
-}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
